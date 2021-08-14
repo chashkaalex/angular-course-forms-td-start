25c02a394ddaa21d370872bc5aa9dd4087ef3d50
chashkaalex <chashkaalex@gmail.com>
2021-08-14 21:31:46 +0300

    final-commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild} from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { mainModule } from 'process';
 
 @Component({
   selector: 'app-root',
@@ -6,7 +8,47 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  @ViewChild('f') singupForm: NgForm;
+  defaultQuestion = 'pet';
+  answer = '';
+  genders = ['male', 'female'];
+  user = {
+    username: '',
+    email: '',
+    secretQuestion: '',
+    answer: '',
+    gender: ''
+  };
+  submitted = false;
   suggestUserName() {
     const suggestedName = 'Superuser';
+    // this.singupForm.setValue({
+    //   userData: {
+    //     username: suggestedName,
+    //     email: ''
+    //   },
+    //   secret: 'pet',
+    //   questionAnswer: '',
+    //   gender: 'male'
+    // });
+    this.singupForm.form.patchValue({
+      userData:{
+        username: suggestedName
+      }
+    });
+  }
+
+  // onSubmit(form: NgForm) {
+  //   console.log(form.form.value);
+  // }
+  onSubmit() {
+    this.user.username = this.singupForm.value.userData.username;
+    this.user.email = this.singupForm.value.userData.email;
+    this.user.secretQuestion = this.singupForm.value.secret;
+    this.user.answer = this.singupForm.value.questionAnswer;
+    this.user.gender = this.singupForm.value.gender;
+    this.submitted = true;
+
+    this.singupForm.reset();
   }
 }
